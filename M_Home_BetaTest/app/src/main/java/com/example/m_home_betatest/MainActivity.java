e7b1b4ae16ec2e957a693c94491eb25af5e5dbfa
junseo0102342gmail.com <junseo010234@gmail.com>
2021-04-05 13:41:38 +0900

    [update] Intent

--- M_Home_BetaTest/app/src/main/java/com/example/m_home_betatest/MainActivity.java
+++ M_Home_BetaTest/app/src/main/java/com/example/m_home_betatest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.m_home_betatest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+
+    public void onClick(View view){
+        switch (view.getId())
+        {
+            case R.id.f:
+                Toast.makeText(this.getApplicationContext(),"현재 페이지 입니다.",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.s:
+                Intent intent = new Intent(this, SubActivity.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
--- M_Home_BetaTest/app/src/main/java/com/example/m_home_betatest/SubActivity.java
+++ M_Home_BetaTest/app/src/main/java/com/example/m_home_betatest/SubActivity.java
@@ -1,4 +1,30 @@
 package com.example.m_home_betatest;
 
-public class SubActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+
+    public void onClick(View view){
+        switch (view.getId())
+        {
+            case R.id.f:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.s:
+                Toast.makeText(this.getApplicationContext(),"현재 페이지 입니다.",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
 }
