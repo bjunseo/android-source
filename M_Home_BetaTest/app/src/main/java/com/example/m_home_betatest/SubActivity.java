6daf7baec79c30ba00e2a01d016349c4e681bdea
junseo0102342gmail.com <junseo010234@gmail.com>
2021-04-08 09:48:39 +0900

    [update] page 2 AUTO, MANUAL mode

--- M_Home_BetaTest/app/src/main/java/com/example/m_home_betatest/SubActivity.java
+++ M_Home_BetaTest/app/src/main/java/com/example/m_home_betatest/SubActivity.java
@@ -4,13 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SubActivity extends AppCompatActivity {
     TextView textView;
+    Switch mySwitch;
     int now_temp = 30;
-
+    boolean sta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,26 +22,51 @@ public class SubActivity extends AppCompatActivity {
 
         textView = (TextView) findViewById(R.id.Temp_V);
         textView.setText(Integer.toString(now_temp));
+
+        TextView t = (TextView) findViewById(R.id.sta);
+
+        mySwitch = (Switch) findViewById(R.id.sta_s);
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked == true)
+                {
+                    sta = true;
+                    t.setText("true");
+                }
+                else
+                {
+                    sta = false;
+                    t.setText("false");
+                }
+            }
+        });
+
+
     }
 
     public void onClick(View view){
-        switch (view.getId())
-        {
-            case R.id.f:
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.s:
-                Toast.makeText(this.getApplicationContext(),"현재 페이지 입니다.",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.Temp_U:
-                now_temp++;
-                textView.setText(Integer.toString(now_temp));
-                break;
-            case R.id.Temp_M:
-                now_temp--;
-                textView.setText(Integer.toString(now_temp));
-                break;
+        if(sta == true) {
+            switch (view.getId()) {
+                case R.id.f:
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.s:
+                    Toast.makeText(this.getApplicationContext(), "현재 페이지 입니다.", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.Temp_U:
+                    now_temp++;
+                    textView.setText(Integer.toString(now_temp));
+                    break;
+                case R.id.Temp_M:
+                    now_temp--;
+                    textView.setText(Integer.toString(now_temp));
+                    break;
+            }
+        }
+        else {
+
         }
     }
 }
