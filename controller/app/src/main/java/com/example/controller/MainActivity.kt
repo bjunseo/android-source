c837503006f322384249d7c60a300f7f214fda42
junseo0102342gmail.com <junseo010234@gmail.com>
2021-04-15 16:46:58 +0900

    [update] controller view

--- controller/app/src/main/java/com/example/controller/MainActivity.kt
+++ controller/app/src/main/java/com/example/controller/MainActivity.kt
@@ -2,17 +2,24 @@ package com.example.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var textView:TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
     }
 
-    fun click()
+    fun click(view: View)
     {
+        when(view)
+        {
+            view  -> {
+                
 
+            }
+        }
     }
 }
